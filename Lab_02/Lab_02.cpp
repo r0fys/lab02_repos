f1ed6be284f70b90bb3a37ee3d574df967afb2d8
r0fys <rofysyt@gmail.com>
2025-09-17 15:04:18 +0300

    com2 new

--- Lab_02/Lab_02.cpp
+++ Lab_02/Lab_02.cpp
@@ -9,7 +9,7 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    string commit = "commit 1 new";
+    string commit = "commit 2 new";
 
     double Pi = 4 * atan(1.);
     double a, z1, z2;
