f4162abd04f0f5fdfd034b751c8b8ebba92e36f8
r0fys <rofysyt@gmail.com>
2025-09-17 13:53:34 +0300

    first commit

---
+++ Lab_02/Lab_02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include "windows.h";
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double Pi = 4 * atan(1.);
+    double a, z1, z2;
+    
+    cout << "Введіть число a: ";
+    cin >> a;
+
+    z1 = (2 * ((sin(3 * Pi - 2 * a)) * (sin(3 * Pi - 2 * a)))) * (cos((5*Pi)+(2*a))* cos((5 * Pi) + (2 * a)));
+    
+    z2 = (1.0/4)-((1.0/4)*(sin((5.0/2)*Pi - 8*a)));
+
+    cout << endl <<"Відповідь рівння 1: " << z1 << endl << "Відповідь рівння 2: " << z2;
+
+    return 0;
+}
+
